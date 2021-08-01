a6827ffa404458c469262d3d5f3505bcc8dda854
vygovskij <v.vygovskij97@gmail.com>
2021-08-01 13:33:01 +0300

    release v2

--- Program.cs
+++ Program.cs
@@ -10,21 +10,28 @@ namespace ConsoleApp8
         private static readonly byte[] SecretKey = new byte[16];
         static void Main(string[] args)
         {
-            
-                Console.Write("Enter the number of items: ");
-            int len = int.Parse(Console.ReadLine());
+            string[] arrayNumber; 
+            int count; 
+            string s;
+            string[] AS2; 
 
-            if (len < 3 || len%2==0)
+            count = 0; 
+            arrayNumber = new string[count]; 
+
+            do
             {
-              
-                Console.WriteLine("Parameter input error");
-                Environment.Exit(0);
-
-            }
-            string[] elements = new string[len];
-
-            Console.Write("Enter the elements: ");
-            string[] arrayNumber = Console.ReadLine().Split(' ');
+                s = Console.ReadLine();
+                if (s != "")
+                {
+                    count++;
+                    AS2 = new string[count];
+                    for (int i = 0; i < AS2.Length - 1; i++)
+                        AS2[i] = arrayNumber[i];
+                    AS2[count - 1] = s;
+                    arrayNumber = AS2;
+                }
+            } while (s != "");
+            string[] elements = new string[count];
             
             for (int i = 0; i < arrayNumber.Length; i++)
             {
@@ -45,15 +52,12 @@ namespace ConsoleApp8
                 Console.WriteLine("{0} - {1}", i + 1, elements[i]);
             Console.WriteLine("0 - Exit");
 
-
                 Console.Write("\nEnter your move: ");
             int userMove = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Your move: {0}", elements[userMove - 1]);
 
             Console.WriteLine("\nComputer move: {0}", pcMove);
-
-            
 
             for (int i = 0; i < elements.Length / 2; i++)
             {
